6eb3814a316580ebe0cbc50ea6ae52c516da8f91
SarikaRAJPURE <sprajpure@gmail.com>
2022-11-19 14:24:44 -0500

    First three loop examples

---
+++ loops.js
@@ -0,0 +1,25 @@
+//Write a for loop that logs 0-15
+
+for(let i=0;i<=15;i++){
+    console.log(i);
+}
+
+//Write a for loop that counts from 15-0
+for(let i=10;i>0;i--){
+    console.log(i);
+}
+
+//Make variable laps = 0; write a for loop that logs (“that’s another lap!”) 
+//and adds 1 to the laps variable. AFTER the loop, log laps.
+
+/* for(let laps=0;laps<10;laps++){
+    console.log("that’s another lap!");
+} */
+
+//using while loop
+laps=0;
+while(laps>10){
+    console.log("that’s another lap!");
+    laps++;
+}
+
